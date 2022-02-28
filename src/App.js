f10d329b3b93954327c1ac7fe7f1e8ad89a756d5
Manav Bajaj <immanav2001@gmail.com>
2022-02-28 12:16:34 +0530

    quiz-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import QuestionList from './QuestionList';
 
 function App() {
+  const[quesNo,setQuesNo]=useState(1);
+  const[showScore,setShowScore]=useState(false);
+  const onSubmit = () =>{
+    if(quesNo<QuestionList.length)
+      setQuesNo(quesNo+1);
+    else
+      setShowScore(true);
+      
+
+  }
+  const[score,setScore]=useState(0);
+  const scoreHandler =(correct) =>{
+     if(correct)
+       setScore(score+1);
+    console.log(score);
+      
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-div">
+      <div className='center-div'>
+      {showScore ?(<div className='result'><div className='status'>Completed!</div><div className='score-display'>Your Score:{score}/{QuestionList.length}</div></div>):(<><div className='question-count'>
+          Question {quesNo} of 5
+        </div>
+        <div className='question'>
+          {QuestionList[quesNo-1].question}
+        </div>
+        <div className='answer-section'>
+         {
+           QuestionList[quesNo-1].answerList.map((answerElem) =>{
+               return(
+                 <li className='answer-list'>
+                   <button className='answer-button' onClick={() =>{scoreHandler(answerElem.isCorrect)}}>{answerElem.answer}</button>
+                 </li>
+                 
+               )
+           })
+         }
+        
+        
+        </div>
+        <button class="next" onClick={onSubmit}>Next</button></>)}
+        
+      </div>
     </div>
   );
 }
---
+++ src/QuestionList.js
@@ -0,0 +1,103 @@
+const QuestionList =[
+    {
+        question:"What is the full form of WWW?",
+        answerList:[
+            {answer:"World Width Web",
+               isCorrect:false
+             },
+             {
+                 answer:"World War Won",
+                 isCorrect:false
+             },
+             {
+                 answer:"World Wide Web",
+                 isCorrect:true
+             },
+             {
+                 answer:"World Web Wide",
+                 isCorrect:false
+             }
+        ]
+    },
+    {
+        question:"What is the capital of USA?",
+        answerList:[
+            {answer:"New York",
+               isCorrect:false
+             },
+             {
+                 answer:"Washington DC",
+                 isCorrect:true
+             },
+             {
+                 answer:"Chicago",
+                 isCorrect:false
+             },
+             {
+                 answer:"Florida",
+                 isCorrect:false
+             }
+        ]
+    },
+    {
+        question:"What is the highest peak in the world?",
+        answerList:[
+            {answer:"Mount Everest",
+               isCorrect:true
+             },
+             {
+                 answer:"Mount K2",
+                 isCorrect:false
+             },
+             {
+                 answer:"Annapurna",
+                 isCorrect:false
+             },
+             {
+                 answer:"Makalu",
+                 isCorrect:false
+             }
+        ]
+    },
+    {
+        question:"Who is the highest earning YouTuber in 2020?",
+        answerList:[
+            {answer:"Mr.Beast",
+               isCorrect:false
+             },
+             {
+                 answer:"David Dobrik",
+                 isCorrect:false
+             },
+             {
+                 answer:"Dude Perfect",
+                 isCorrect:false
+             },
+             {
+                 answer:"Ryan Kaji",
+                 isCorrect:true
+             }
+        ]
+    },
+    {
+        question:"Who maintains React?",
+        answerList:[
+            {answer:"Google",
+               isCorrect:false
+             },
+             {
+                 answer:"Amazon",
+                 isCorrect:false
+             },
+             {
+                 answer:"Facebook",
+                 isCorrect:true
+             },
+             {
+                 answer:"Apple",
+                 isCorrect:false
+             }
+        ]
+    }
+];
+export default QuestionList;
